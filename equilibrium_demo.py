a10342bcfef335b93dec07893c8da7ace4c4339a
Tung Thanh Le <ttungl@users.noreply.github.com>
2017-03-16 20:01:41 -0500

    Update equilibrium_demo.py

--- equilibrium_demo.py
+++ equilibrium_demo.py
@@ -2,18 +2,14 @@ def Equilibrium(A):
 	sum_all = 0
 	Rsum = 0
 	Lsum = 0
-	n = len(A)
 
 	for (i,v) in enumerate(A):
 		sum_all += A[i]
 	for (j,w) in enumerate(A):
 		Rsum = sum_all - Lsum - A[j]
 		if Rsum == Lsum:
-			print j
 			return j
 		Lsum += A[j]
-
-	print -1
 	return -1
 
 # Tests
