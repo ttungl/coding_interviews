e201128ea6ef3c2ed1dc9881fc10e4b704c06005
Tung Thanh Le <ttungl@users.noreply.github.com>
2017-03-18 16:13:38 -0500

    Add files via upload

---
+++ BinaryGap.py
@@ -0,0 +1,37 @@
+
+def solution(N):
+	BinArray = []
+	Array = []
+	flag = 0
+	counter=0
+	## convert N to binary
+	while not N==0:
+		if N%2==0:
+			BinArray.append(0)
+		else:
+			BinArray.append(1)
+		
+		N = N/2 ## update N
+	
+	## reverse BinArray
+	BinArray.reverse()
+	
+	## counter for the gap
+	for i in BinArray:
+		if i==1:
+			if flag==0:
+				flag=1
+			else:
+				Array.append(counter)
+				counter=0
+
+		else:
+			if flag==1:
+				counter=counter+1
+	# print Array
+	# print max(Array)
+	## return
+	return max(Array)
+	
+N=1041
+solution(N)
